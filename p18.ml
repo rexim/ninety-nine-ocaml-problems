311f36dc57e7990e5377f5694a9f3ffcf9831660
rexim <reximkut@gmail.com>
2013-09-27 09:53:34 +0700

    Solved Problem 18.

---
+++ p18.ml
@@ -0,0 +1,38 @@
+(* Problem 18.
+ *
+ * Extract a slice from a list.
+ 
+ * Given two indices, `i` and `k`, the slice is the list containing
+ * the elements between the i'th abnd k'th elements of the original
+ * list (both limits included). Start counting the elements with 0
+ * (this is way the `List` module numbers elements. *)
+
+(* Solution *)
+
+let slice xs i k =
+  let rec drop xs n =
+    match xs with
+    | [] -> []
+    | _ :: ts -> if n = 0
+                 then xs
+                 else drop ts (n - 1)
+  in
+  let rec take xs n =
+    match xs with
+    | [] -> []
+    | t :: ts -> if n = 0
+                 then []
+                 else t :: take ts (n - 1)
+  in
+  take (drop xs i) (k - i + 1);;
+
+(* Testing *)
+
+let test1 () =
+  let xs = [`a;`b;`c;`d;`e;`f;`g;`h;`i;`j] in
+  let i = 2 in
+  let k = 6 in
+  let expected = [`c; `d; `e; `f; `g] in
+  assert (expected = slice xs i k);;
+
+List.iter (fun f -> f ()) [test1];;
